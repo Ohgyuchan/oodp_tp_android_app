41bb18388ea851b62c1d1fa161a5d046c1f5072d
Ohgyuchan <gyu3637@gmail.com>
2022-05-06 09:16:40 +0900

    TaskAdapter

---
+++ app/src/main/java/com/example/oodp_tp_app/activity/ProjectDetail.java
@@ -0,0 +1,16 @@
+package com.example.oodp_tp_app.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.oodp_tp_app.R;
+
+public class ProjectDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_detail);
+    }
+}
--- app/src/main/java/com/example/oodp_tp_app/activity/TaskCreateActivity.java
+++ app/src/main/java/com/example/oodp_tp_app/activity/TaskCreateActivity.java
@@ -1,15 +1,48 @@
 package com.example.oodp_tp_app.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.oodp_tp_app.R;
 
 public class TaskCreateActivity extends AppCompatActivity {
+    private EditText et;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task_create);
+
+        et = findViewById(R.id.et_task_name);
+        btn = findViewById(R.id.btn_task_add);
+
+        Intent intent = getIntent();
+
+        if (intent.getStringExtra("mode") == "project"){
+            et.setText("Project Name");
+
+        } else {
+            et.setText("Create Name");
+        }
+
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (intent.getStringExtra("mode") == "project"){
+                    Intent intent = new Intent(getApplicationContext(), TaskCreateActivity.class);
+
+                } else {
+                    et.setText("Create Name");
+                }
+            }
+        });
     }
 }
